24180003a07e081df5ba52dceb899bcf4e1d57f9
searraf <54295550+searraf@users.noreply.github.com>
2022-04-14 17:32:28 -0700

    initial commit

---
+++ AppMenuAR/SideMenuModel.swift
@@ -0,0 +1,15 @@
+//
+//  MenuModel.swift
+//  AppMenuAR
+//
+//  Created by seseadmin on 4/6/22.
+//
+
+import Foundation
+import UIKit
+
+struct SideMenuModel {
+    var icon: UIImage
+    var title: String
+}
+
